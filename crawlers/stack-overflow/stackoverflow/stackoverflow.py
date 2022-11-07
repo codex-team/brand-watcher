2f9d21032d8c282c25c09ad069efb528d97bd887
slaveeks <chernyshev2001@icloud.com>
2022-11-07 11:14:43 +0300

    Some dockerfile changes, updated .gitignore, added docker-compose file with redis and stackoverflow crawler

--- crawlers/stack-overflow/stackoverflow/stackoverflow.py
+++ crawlers/stack-overflow/stackoverflow/stackoverflow.py
@@ -11,7 +11,7 @@ class StackOverflowCrawler:
     :param keywords - keywords to find
     :param redis_host - host for redis database
     :param redis_port- port for redis database
-    :param redis_password - password for redis databse
+    :param redis_password - password for redis database
     """
 
     def __init__(self, keywords: list[str], redis_host: str, redis_port: int, redis_password: str = ''):
@@ -33,10 +33,15 @@ class StackOverflowCrawler:
             hashed_question_url = Utils.hash_data(question.url)
             res = self.db.find_data(hashed_question_url)
 
+            print(f"Founded data: {res}")
+
             if not res:
                 # Convert dict data to string
                 data = json.dumps(question.to_dict())
 
                 self.db.save_data(hashed_question_url, data)
+
+                print(f"New data: {data}")
+
 
 
