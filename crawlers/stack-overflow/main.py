0dc797c2c2a10248797d314b17bfa441d3d8706a
slaveeks <chernyshev2001@icloud.com>
2022-11-17 15:12:47 +0300

    Removed crawler_name variable from main file

--- crawlers/stack-overflow/main.py
+++ crawlers/stack-overflow/main.py
@@ -13,8 +13,6 @@ CRAWLER_NAME = 'stack-overflow'
 if __name__ == '__main__':
     config = Utils.load_json_file('config.json')
 
-    crawler_name = 'stack-overflow'
-
     db = Db(config['redis-url'], CRAWLER_NAME)
     broker = Broker(config['rabbitmq-url'], CRAWLER_NAME)
 
