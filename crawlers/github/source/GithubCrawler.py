05ff6a19912c9dbb637a1d9aa570e6a235169522
tronglam.itmo <tronglam.itmo@gmail.com>
2022-11-19 13:45:02 +0300

    update crawl's github method, replace all double quotes, restructure comment

--- crawlers/github/source/GithubCrawler.py
+++ crawlers/github/source/GithubCrawler.py
@@ -6,7 +6,7 @@ import logging
 from source.db.db import Db
 from source.utils.utils import Utils
 
-BASE_URL = "https://api.github.com/search/repositories"
+BASE_URL = 'https://api.github.com/search/repositories'
 
 """
 Crawler has 2 properties is base URL of GithubAPI 
@@ -16,17 +16,19 @@ and redis's database information.
 class GithubCrawler:
 
     def __init__(self, cache: Db, name: str = 'GithubCrawler' ):
+        '''
+        :param `name`: name of crawler
+        :param `cache`: redis database
+        '''
         self.name = name
         self.cache = cache
 
-    """
-    Define crawl function, which accept keyword from
-    config file and make request to github API to get 
-    coresponding data.
-    """
-
     def _save_to_cache(self, keyword, data):
-        # Check if keyword already in cache or not
+        ''' Check if keyword already in cache or not. If not, then update
+
+        :param `keyword`: keyword from config file to get the realated data
+        :param `data`: the data we got after make request
+        '''
         for item in data:
             key = f'{self.name}:{keyword}'
             id = item['url']
@@ -34,20 +36,32 @@ class GithubCrawler:
                 self.cache.add_to_set(key, id)
 
     def crawl(self, keyword):
+        ''' Make request to github API in order to crawl related data with keyword
+
+        :param `keyword`: keyword from config file to get the related data
+        '''
         data = []
         rq = f'{BASE_URL}?q={keyword}' 
         response = requests.get(rq).json()
         if len(response) == 0:
-            logging.info(f"No data match with keyword: {keyword}")
+            logging.info(f'No data match with keyword: {keyword}')
         else:
             items = response["items"]
-            for i in range(len(items)):
+            '''
+            Pasre raw data to usefull data
+            `repo_full_name`: name of repository in github
+            `url`: html url of the repository
+            `owner`: repository's owner 
+            `description`: description about repository
+            `language`: language is used to written repository          
+            '''
+            for item in items:
                 tmp = {}
-                tmp["repo_full_name"] = items[i]["full_name"]
-                tmp["url"] = items[i]["html_url"]
-                tmp["owner"] = items[i]["owner"]["login"]
-                tmp["description"] = items[i]["description"]
-                tmp["language"] = items[i]["language"]
+                tmp['repo_full_name'] = item['full_name']
+                tmp['url'] = item['html_url']
+                tmp['owner'] = item['owner']['login']
+                tmp['description'] = item['description']
+                tmp['language'] = item['language']
                 data.append(tmp)
 
         self._save_to_cache(keyword, data)
