d56d4110a2c19b203759bd32da5a6b49769d6f2d
hoang <hoangnd997@gmail.com>
2022-12-01 19:34:57 +0300

    move log initiate to first line

--- collector/main.go
+++ collector/main.go
@@ -9,9 +9,9 @@ import (
 )
 
 func main() {
+	log := logger.GetLogger()
 	config := utils.ReadConfigFile("./config.json")
 
 	rabbitmq.ConnectToRabbitMQ(config.RabbitMQUrl)
-	log := logger.GetLogger()
 	log.Info(fmt.Sprintf("Connected to rabbitmq with url: %s", config.RabbitMQUrl))
 }
