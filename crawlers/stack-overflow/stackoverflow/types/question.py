2540f7cdae8dabd2858259927d4512708923453f
slaveeks <chernyshev2001@icloud.com>
2022-12-01 13:46:04 +0300

    Changed structure of sending message to rabbitmq in stack-overflow

--- crawlers/stack-overflow/stackoverflow/types/question.py
+++ crawlers/stack-overflow/stackoverflow/types/question.py
@@ -8,6 +8,8 @@ API_URL = 'https://api.stackexchange.com/2.3/search'
 
 T = TypeVar('T', bound='Question')
 
+FILTER = '!nOedRLezYU'
+
 
 class Question:
     """
@@ -18,9 +20,10 @@ class Question:
     :param date - question creation date
     :param author - question author name
     :param keyword - founded keyword
+    :param body - question body
     """
 
-    def __init__(self, question_id, title: str, url: str, date, author: str, keyword: str):
+    def __init__(self, question_id, title: str, url: str, date, author: str, keyword: str, body: str):
         self.question_id = question_id
         self.title = title
         self.url = url
@@ -28,6 +31,7 @@ class Question:
         self.author = author
         self.answers: list[Answer] = []
         self.keyword = keyword
+        self.body = body
 
     @classmethod
     def find_by_tag(cls: Type[T], tag: str) -> list[T]:
@@ -45,6 +49,7 @@ class Question:
             "sort": "activity",
             "site": "stackoverflow",
             "tagged": tag,
+            "filter": FILTER,
         }
 
         res = requests.get(API_URL, params=params)
@@ -52,7 +57,7 @@ class Question:
             # Check founded data
             for item in res.json()['items']:
                 question = cls(item['question_id'], item['title'], item['link'], item['creation_date'],
-                               item['owner']['display_name'], tag)
+                               item['owner']['display_name'], tag, item['body'])
 
                 questions.append(question)
         except Exception as e:
@@ -70,14 +75,18 @@ class Question:
 
         # Convert to dict the list of answers
         for answer in self.answers:
-            answers.append(answer.to_dict())
+            answers.append(answer.title)
 
         return {
+            "id": self.question_id,
             "title": self.title,
             "url": self.url,
             "date": self.date,
             "author": self.author,
-            "answers": answers
+            "body": self.body,
+            "comments": answers,
+            "keyword": self.keyword,
+            "source": 'stack-overflow'
         }
 
 
